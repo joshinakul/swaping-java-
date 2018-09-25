8ead61632085bdeaa453239c1b38079ee6bda675
Nakul Joshi <36658319+joshinakul@users.noreply.github.com>
2018-09-25 10:34:33 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+class Swap
+{
+      public static void main(String args[])
+      {
+       int a=2,b=3,t;
+	System.out.println("before swapping values are " + a +" "+ b);
+	t=a;
+	a=b;
+	b=t;
+	System.out.println("after swapping values are " + a +" "+ b);
+	}
+}
